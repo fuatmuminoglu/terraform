7fd7196f0fb410fc3907d04422eb5033b2a1b520
fuatmuminoglu <fuatmuminoglu@gmail.com>
2020-08-23 08:34:04 +0100

    2 instance with Ngnix + 1 loadbalancer

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_elb" "web" {
 
   subnets         = [aws_subnet.default.id]
   security_groups = [aws_security_group.elb.id]
-  instances       = [aws_instance.web.id]
+  instances       = [aws_instance.web.id,aws_instance.web2.id]
 
   listener {
     instance_port     = 80
@@ -115,7 +115,6 @@ resource "aws_instance" "web" {
     host = self.public_ip
     # The connection will use the local SSH agent for authentication.
   }
-
   instance_type = "t2.micro"
 
   # Lookup the correct AMI based on the region
@@ -144,3 +143,44 @@ resource "aws_instance" "web" {
     ]
   }
 }
+
+resource "aws_instance" "web2" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    type = "ssh"
+    # The default username for our AMI
+    user = "ubuntu"
+    host = self.public_ip
+    # The connection will use the local SSH agent for authentication.
+  }
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = var.aws_amis[var.aws_region]
+
+  # The name of our SSH keypair we created above.
+  key_name = aws_key_pair.auth.id
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
+  }
+}
+
+
--- terraform.template.tfvars
+++ terraform.template.tfvars
@@ -1,2 +1,2 @@
 key_name        = "terraform-provider-aws-example"
-public_key_path = "~/.ssh/terraform-provider-aws-example.pub"
+public_key_path = "~/.ssh/id_rsa.pub"
